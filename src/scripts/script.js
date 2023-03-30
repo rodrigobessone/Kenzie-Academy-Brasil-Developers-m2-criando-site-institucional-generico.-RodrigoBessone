054359acacf2a6e569a5a48e3a4ce58beaaec1c1
rodrigobessone <rmb@ecomp.poli.br>
2023-03-30 14:21:03 -0300

    Entrega feita.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,38 @@
 /* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+
+openModal()
+
+closeModal()
+
+arrowDirection()
+
+// Modal
+function openModal() {
+    const dialog = document.querySelector(".modal")
+    const btnsToOpenModal = document.querySelectorAll(".open-modal")
+
+    btnsToOpenModal.forEach(btn => {
+        btn.addEventListener("click", () => {
+            dialog.showModal()
+        })
+    })
+}
+
+function closeModal() {
+    const dialog = document.querySelector(".modal")
+    dialog.close()
+}
+//Arrow Direction FAQ
+function arrowDirection(){
+const arrows = document.querySelectorAll(".main__forth-summary")
+
+arrows.forEach(arrow =>{
+    arrow.addEventListener("click", () =>{
+        arrow.classList.toggle("main__forth-summary--open")
+    })
+})
+
+}
+
+
+
